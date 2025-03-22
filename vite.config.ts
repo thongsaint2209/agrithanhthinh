f1a186d491b2b2a08013f34c8486f69d9e57e5c3
Thong <quangthongsainto@gmail.com>
2025-03-22 23:24:37 +0700

    fix 404

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,14 @@ import { defineConfig } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
+
   server: {
     port: 3000,
   },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  // 🔥 Fix lỗi refresh bị 404 trên Vercel
-  esbuild: {
-    jsxInject: `import React from 'react'`,
   },
 });
